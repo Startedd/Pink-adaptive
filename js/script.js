b3c62c9cd0f5afde790f1f42b88e77879772c2d5
DenysKuts <crudelis2@gmail.com>
2017-06-28 20:17:50 +0300

    Stylistic changes enforced by ESlint

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
-let map;
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function () {
-
-    if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
-    } else {
-        navMain.classList.remove('main-nav--opened');
-        navMain.classList.add('main-nav--closed');
-    }
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+  }
 });
-
-(function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 59.936253, lng: 30.320740},
-        zoom: 16
-    });
-
-    let image = 'img-optimized/icon-map-marker.svg';
-    let marker = new google.maps.Marker({
-        position: {lat: 59.936253, lng: 30.320740},
-        map: map,
-        title: 'Hello World!',
-        icon: image
-    });
-})();
-
 
